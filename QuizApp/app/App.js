81847d033f85e5481d994cd07829a50a3e0a3772
tuna <tunactnkya@gmail.com>
2023-01-05 17:10:43 +0300

    Mini Quiz with React Native

---
+++ QuizApp/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from 'react-native';
+import Quiz from "./screens/Quiz";
+
+function App() {
+  return (
+
+    <Quiz />
+  )
+}
+
+export default App;
---
+++ QuizApp/app/data/QuizData.js
@@ -0,0 +1,38 @@
+export default data = [
+    {
+        question: "Where is the largest river in the world?",
+        options: ["Nil", "Amazon", "Yangtze", "Lena"],
+        correct_option: "Amazon"
+    },
+
+    {
+        question: "Which country has the most populous population?",
+        options: ["China", "USA", "India", "Germany"],
+        correct_option: "China"
+    },
+
+    {
+        question: "What is the largest animal on Earth?",
+        options: ["The African Elephant", "The Blue Whale", "The Sperm Whale", "The Giant Squid"],
+        correct_option: "The Blue Whale"
+    },
+
+    {
+        question: "What is the oldest civilization?",
+        options: ["Sumerians", "Persian Empire", "Assyrians", "Babylonians"],
+        correct_option: "Sumerians"
+    },
+
+    {
+        question: "Who is the player with the most trophies?",
+        options: ["Lionel Messi", "Cristiano Ronaldo", "David Beckham", "Dani Alves"],
+        correct_option: "Dani Alves"
+    },
+
+
+
+
+
+
+
+]
